7709f52e80eefd4bd179f2bec3f3d9aef6cbaf99
kayo silva <kayo.vinicius0905@gmail.com>
2023-11-09 12:50:17 -0300

    colocando a indicação de segurança na barra

--- script.js
+++ script.js
@@ -43,10 +43,24 @@ function generatePassword(){
 }
 
 function calculateQuality() {
+    const percent =  Math.round((passwordLength / 64) * 25 + (upperCaseEl.checked ? 15 : 0) + (numberCaseEl.checked ? 25 : 0 ) + (symbolCaseEl.checked ? 35 : 0 ))
 
-    
-    const percent =  Math.round((passwordLength / 64) * 100)
     securityIndicatorBarEl.style.width = `${percent}%`
+
+    if (percent > 69){
+        securityIndicatorBarEl.classList.remove("critical")
+        securityIndicatorBarEl.classList.remove("warning")
+        securityIndicatorBarEl.classList.add("safe")
+    }else if (percent > 20){
+        securityIndicatorBarEl.classList.remove("critical")
+        securityIndicatorBarEl.classList.add("warning")
+        securityIndicatorBarEl.classList.remove("safe")
+    }else {
+        securityIndicatorBarEl.classList.add("critical")
+        securityIndicatorBarEl.classList.remove("warning")
+        securityIndicatorBarEl.classList.remove("safe")
+    }
+    
 }
 
 function copy(){
